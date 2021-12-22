24dd640106ffd5049d358f59f93938c9baa0f9e9
efortish <91025555+efortish@users.noreply.github.com>
2021-12-22 16:27:36 -0500

    users_show_all

--- main.py
+++ main.py
@@ -123,7 +123,25 @@ def login():
     tags= ["Users"]
 )
 def show_all_users():
-    pass
+    """
+    Show all users
+
+    This path operation shows all users registered in the app
+
+    Parameters:
+    - None 
+
+    Returns a json list with all users in the app, with the following keys
+    - user_id: UUID
+    - email: Emailstr
+    - first_name: str
+    - last_name: str
+    - birth_date: str
+
+    """
+    with open ("users.json", "r", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        return results
 
 ### Show a user
 @app.get(
